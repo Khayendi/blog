374efd7263c9117a2ed1da4ad9677ae39e6666d1
Khayendi <khayendi00@gmail.com>
2024-08-11 14:56:53 +0300

    Add route configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
-import org.gradle.internal.impldep.org.apache.commons.io.output.ByteArrayOutputStream
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.io.ByteArrayOutputStream
 import java.nio.charset.Charset
 
 plugins {
     kotlin("jvm") version "1.9.22"
     application
     id("io.vertx.vertx-plugin") version "1.3.0"
+//    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 fun getVersionName():Any{
@@ -21,7 +22,7 @@ fun getVersionName():Any{
     }
 }
 
-group = "com.rose"
+group = "com.rose.blog"
 version = getVersionName()
 
 repositories {
@@ -29,7 +30,7 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("io.vertx:vertx-core:4.4.4"))
+    implementation(platform("io.vertx:vertx-core:4.5.9"))
     implementation("io.vertx:vertx-web")
     implementation("io.vertx:vertx-mongo-client")
     implementation("io.vertx:vertx-mail-client")
@@ -38,11 +39,11 @@ dependencies {
     implementation("org.slf4j:slf4j-api:2.0.7")
     implementation("io.vertx:vertx-auth-oauth2:4.4.4")
     implementation("commons-logging:commons-logging:1.2")
-    implementation("org.springframework.security:spring-security-crypto:5.5.4")
-    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.10.0"))
+    implementation("org.springframework.security:spring-security-crypto:5.6.4")
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
     implementation("com.squareup.okhttp3:okhttp")
     implementation("com.squareup.okhttp3:logging-interceptor")
-    implementation("org.json:json:20230227")
+    implementation("org.json:json:20231013")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.2")
     testImplementation("io.vertx:vertx-junit5")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.2")
@@ -69,8 +70,4 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
-}
-
-application {
-    mainClass.set("MainKt")
 }
--- src/main/kotlin/com/rose/blog/BlogService.kt
+++ src/main/kotlin/com/rose/blog/BlogService.kt
@@ -1,4 +1,73 @@
 package com.rose.blog
 
-class BlogService {
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.Promise
+import io.vertx.core.http.HttpClientResponse
+import io.vertx.core.http.HttpMethod
+import io.vertx.core.impl.logging.LoggerFactory
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.handler.CorsHandler
+
+class BlogService(private val port:Int=System.getenv("APP_PORT").toInt()):AbstractVerticle() {
+    private val logger = LoggerFactory.getLogger(this.javaClass.simpleName)
+    override fun start(startPromise: Promise<Void>?) {
+        super.start(startPromise)
+        val router = Router.router(this.vertx)
+        this.vertx.createHttpServer()
+            .requestHandler(router)
+            .listen(this.port){
+                if (it.succeeded()){
+                    this.logger.info("Server started on port: $port")
+                    startPromise?.complete()
+                    setRoutes(router)
+                }else{
+                    this.logger.error("Server failed start.")
+                    startPromise?.fail(it.cause())
+                }
+            }
+    }
+
+    override fun stop(stopPromise: Promise<Void>?) {
+        super.stop(stopPromise)
+        this.vertx.close{
+            if (it.succeeded()){
+                this.logger.info("Server stopped successfully.")
+                stopPromise?.complete()
+            }else{
+                this.logger.error("Server failed to stop.")
+                stopPromise?.fail(it.cause())
+            }
+        }
+    }
+
+    private fun setRoutes(router: Router){
+        router.route()
+            .handler(
+            CorsHandler.create()
+                .allowCredentials(true)
+                .allowedHeaders(System.getenv("ALLOWED_HEADERS").split(",").toSet())
+                .allowedMethods(System.getenv("ALLOWED_METHODS").split(",").map { it.toHttpMethod() }.onEach { this.logger.info("${it?.name()} method registered successfully.") }.toSet()))
+
+    }
+
+
+
+
+}
+
+
+fun String.toHttpMethod(): HttpMethod? {
+    return if (this.uppercase() == "POST")
+        HttpMethod.POST
+    else if (this.uppercase() == "GET")
+        HttpMethod.GET
+    else if (this.uppercase() == "PUT")
+        HttpMethod.PUT
+    else if (this.uppercase() == "PATCH")
+        HttpMethod.PATCH
+    else if (this.uppercase() == "OPTIONS")
+        HttpMethod.OPTIONS
+    else if (this.uppercase() == "DELETE")
+        HttpMethod.DELETE
+    else HttpMethod.GET
 }
